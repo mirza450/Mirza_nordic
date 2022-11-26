c8942c302fc490da5ea01be0eec664254a0a5c86
Mirza Alam Begg <mirzaalam450@gmail.com>
2022-11-26 21:17:27 +0530

    html testing in jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { createRoot } from 'react-dom/client';
+const red1="red";
+const yellow1="yellow";
+const blue1 ="green";
+function App(){
+    return (
+    <Fragment>
+    <div>
+    <h1 
+    style={{
+        backgroundColor:blue1,
+        color:yellow1,
+    }} >Hello React </h1>
+    <h1
+    style={{
+        backgroundColor:red1,
+        color:blue1
+    }}>Hello World !</h1>
+    </div>
+    <div>
+    <input type="text"/>
+    </div>
+    </Fragment>
+    );
+}
+
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<h1> Hello react </h1>);
+root.render(<>
+<App />
+<App />
+<App />
+</>);
